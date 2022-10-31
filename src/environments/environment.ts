fec7cee3c0fe4340fba72aaa2d7439c9e0742820
k-dev-web <redjonaldo@gmail.com>
2022-10-31 15:53:23 +0200

    change for heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:3000',
-  apiUrl: ' https://color-ext-back.herokuapp.com/'
+  //apiUrl: 'https://localhost:3000',
+  apiUrl: ' https://color-ext-back.herokuapp.com'
 };
